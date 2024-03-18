b4ac74b9c1cceff9e352329c91c7bef283c7923a
paul <yewhonwa@gmail.com>
2024-03-19 07:50:37 +0800

    Templates

---
+++ src/games/template.ts
@@ -0,0 +1,312 @@
+import { GameBase, IAPGameState, IClickResult, IIndividualState, IValidationResult } from "./_base";
+import { APGamesInformation } from "../schemas/gameinfo";
+import { APRenderRep } from "@abstractplay/renderer/src/schemas/schema";
+import { APMoveResult } from "../schemas/moveresults";
+import { reviver, UserFacingError } from "../common";
+import i18next from "i18next";
+
+type playerid = 1 | 2;
+
+interface IMoveState extends IIndividualState {
+    currplayer: playerid;
+    board: Map<string, playerid>;
+    lastmove?: string;
+}
+
+export interface ITemplState extends IAPGameState {
+    winner: playerid[];
+    stack: Array<IMoveState>;
+};
+
+export class TemplGame extends GameBase {
+    public static readonly gameinfo: APGamesInformation = {
+        name: "Templ",
+        uid: "templ",
+        playercounts: [2],
+        version: "20240125",
+        dateAdded: "2024-02-13",
+        // i18next.t("apgames:descriptions.templ")
+        description: "apgames:descriptions.templ",
+        urls: [],
+        people: [],
+        variants: [],
+        categories: ["goal>connect", "mechanic>place", "board>shape>rect", "board>connect>rect", "components>simple"],
+        flags: ["experimental"],
+    };
+
+    public coords2algebraic(x: number, y: number): string {
+        return GameBase.coords2algebraic(x, y, this.boardSize);
+    }
+
+    public algebraic2coords(cell: string): [number, number] {
+        return GameBase.algebraic2coords(cell, this.boardSize);
+    }
+
+    public numplayers = 2;
+    public currplayer!: playerid;
+    public board!: Map<string, playerid>;
+    public gameover = false;
+    public winner: playerid[] = [];
+    public stack!: Array<IMoveState>;
+    public results: Array<APMoveResult> = [];
+    public variants: string[] = [];
+    private boardSize = 0;
+    private dots: string[] = [];
+
+    constructor(state?: ITemplState | string, variants?: string[]) {
+        super();
+        if (state === undefined) {
+            if (variants !== undefined) {
+                this.variants = [...variants];
+            }
+            const fresh: IMoveState = {
+                _version: TemplGame.gameinfo.version,
+                _results: [],
+                _timestamp: new Date(),
+                currplayer: 1,
+                board: new Map(),
+            };
+            this.stack = [fresh];
+        } else {
+            if (typeof state === "string") {
+                state = JSON.parse(state, reviver) as ITemplState;
+            }
+            if (state.game !== TemplGame.gameinfo.uid) {
+                throw new Error(`The Templ game code cannot process a game of '${state.game}'.`);
+            }
+            this.gameover = state.gameover;
+            this.winner = [...state.winner];
+            this.variants = state.variants;
+            this.stack = [...state.stack];
+        }
+        this.load();
+    }
+
+    public load(idx = -1): TemplGame {
+        if (idx < 0) {
+            idx += this.stack.length;
+        }
+        if (idx < 0 || idx >= this.stack.length) {
+            throw new Error("Could not load the requested state from the stack.");
+        }
+
+        const state = this.stack[idx];
+        if (state === undefined) {
+            throw new Error(`Could not load state index ${idx}`);
+        }
+        this.results = [...state._results];
+        this.currplayer = state.currplayer;
+        this.board = new Map(state.board);
+        this.lastmove = state.lastmove;
+        this.boardSize = this.getBoardSize();
+        return this;
+    }
+
+    private getBoardSize(): number {
+        // Get board size from variants.
+        if (this.variants !== undefined && this.variants.length > 0 && this.variants[0] !== undefined && this.variants[0].length > 0) {
+            const sizeVariants = this.variants.filter(v => v.includes("size"))
+            if (sizeVariants.length > 0) {
+                const size = sizeVariants[0].match(/\d+/);
+                return parseInt(size![0], 10);
+            }
+            if (isNaN(this.boardSize)) {
+                throw new Error(`Could not determine the board size from variant "${this.variants[0]}"`);
+            }
+        }
+        return 4;
+    }
+
+    public moves(player?: playerid): string[] {
+        if (player === undefined) {
+            player = this.currplayer;
+        }
+        if (this.gameover) { return []; }
+        const moves: string[] = [];
+        return moves;
+    }
+
+    public randomMove(): string {
+        const moves = this.moves();
+        return moves[Math.floor(Math.random() * moves.length)];
+    }
+
+    public handleClick(move: string, row: number, col: number, piece?: string): IClickResult {
+        try {
+            const cell = this.coords2algebraic(col, row);
+            let newmove = "";
+            newmove = cell;
+            const result = this.validateMove(newmove) as IClickResult;
+            if (!result.valid) {
+                result.move = "";
+            } else {
+                result.move = newmove;
+            }
+            return result;
+        } catch (e) {
+            return {
+                move,
+                valid: false,
+                message: i18next.t("apgames:validation._general.GENERIC", { move, row, col, piece, emessage: (e as Error).message })
+            }
+        }
+    }
+
+    public validateMove(m: string): IValidationResult {
+        const result: IValidationResult = {valid: false, message: i18next.t("apgames:validation._general.DEFAULT_HANDLER")};
+        if (m.length === 0) {
+            result.valid = true;
+            result.complete = -1;
+            result.canrender = true;
+            result.message = i18next.t("apgames:validation.templ.INITIAL_INSTRUCTIONS");
+            return result;
+        }
+        result.valid = true;
+        result.complete = 1;
+        result.message = i18next.t("apgames:validation._general.VALID_MOVE");
+        return result;
+    }
+
+    public move(m: string, {partial = false, trusted = false} = {}): TemplGame {
+        if (this.gameover) {
+            throw new UserFacingError("MOVES_GAMEOVER", i18next.t("apgames:MOVES_GAMEOVER"));
+        }
+
+        let result;
+        m = m.toLowerCase();
+        m = m.replace(/\s+/g, "");
+        if (!trusted) {
+            result = this.validateMove(m);
+            if (!result.valid) {
+                throw new UserFacingError("VALIDATION_GENERAL", result.message);
+            }
+            if (!partial && !this.moves().includes(m)) {
+                throw new UserFacingError("VALIDATION_FAILSAFE", i18next.t("apgames:validation._general.FAILSAFE", { move: m }));
+            }
+        }
+        if (m.length === 0) { return this; }
+        this.dots = [];
+        this.results = [];
+        this.results.push({ type: "place", where: m });
+        this.board.set(m, this.currplayer);
+
+        this.lastmove = m;
+        this.currplayer = this.currplayer % 2 + 1 as playerid;
+
+        this.checkEOG();
+        this.saveState();
+        return this;
+    }
+
+    protected checkEOG(): TemplGame {
+        const otherPlayer = this.currplayer % 2 + 1 as playerid;
+        if (false) {
+            this.gameover = true;
+            this.winner = [otherPlayer];
+        }
+        if (this.gameover) {
+            this.results.push({ type: "eog" });
+            this.results.push({ type: "winners", players: [...this.winner] });
+        }
+        return this;
+    }
+
+    public state(): ITemplState {
+        return {
+            game: TemplGame.gameinfo.uid,
+            numplayers: 2,
+            variants: this.variants,
+            gameover: this.gameover,
+            winner: [...this.winner],
+            stack: [...this.stack],
+        };
+    }
+
+    protected moveState(): IMoveState {
+        return {
+            _version: TemplGame.gameinfo.version,
+            _results: [...this.results],
+            _timestamp: new Date(),
+            currplayer: this.currplayer,
+            lastmove: this.lastmove,
+            board: new Map(this.board),
+        };
+    }
+
+    public render(): APRenderRep {
+        // Build piece string
+        let pstr = "";
+        for (let row = 0; row < this.boardSize; row++) {
+            if (pstr.length > 0) {
+                pstr += "\n";
+            }
+            for (let col = 0; col < this.boardSize; col++) {
+                const cell = this.coords2algebraic(col, row);
+                if (this.board.has(cell)) {
+                    const contents = this.board.get(cell);
+                    if (contents === 1) {
+                        pstr += "A";
+                    } else if (contents === 2) {
+                        pstr += "B";
+                    }
+                } else {
+                    pstr += "-";
+                }
+            }
+        }
+        pstr = pstr.replace(new RegExp(`-{${this.boardSize}}`, "g"), "_");
+
+        // Build rep
+        const rep: APRenderRep =  {
+            board: {
+                style: "squares",
+                width: this.boardSize,
+                height: this.boardSize,
+            },
+            legend: {
+                A: [{ name: "piece", player: 1 }],
+                B: [{ name: "piece", player: 2 }],
+            },
+            pieces: pstr,
+        };
+
+        // @ts-ignore
+        rep.annotations = [];
+        if (this.results.length > 0) {
+            for (const move of this.results) {
+                if (move.type === "place") {
+                    const [x, y] = this.algebraic2coords(move.where!);
+                    rep.annotations.push({ type: "enter", targets: [{ row: y, col: x }] });
+                } else if (move.type === "move") {
+                    const [fromX, fromY] = this.algebraic2coords(move.from);
+                    const [toX, toY] = this.algebraic2coords(move.to);
+                    rep.annotations.push({ type: "move", targets: [{ row: fromY, col: fromX }, { row: toY, col: toX }] });
+                }
+            }
+        }
+        if (this.dots.length > 0) {
+            const points = [];
+            for (const cell of this.dots) {
+                const [x, y] = this.algebraic2coords(cell);
+                points.push({ row: y, col: x });
+            }
+            // @ts-ignore
+            rep.annotations.push({ type: "dots", targets: points });
+        }
+        return rep;
+    }
+
+    public status(): string {
+        let status = super.status();
+
+        if (this.variants !== undefined) {
+            status += "**Variants**: " + this.variants.join(", ") + "\n\n";
+        }
+
+        return status;
+    }
+
+    public clone(): TemplGame {
+        return new TemplGame(this.serialize());
+    }
+}
---
+++ src/games/template_connection.ts
@@ -0,0 +1,390 @@
+import { GameBase, IAPGameState, IClickResult, IIndividualState, IValidationResult } from "./_base";
+import { APGamesInformation } from "../schemas/gameinfo";
+import { APRenderRep } from "@abstractplay/renderer/src/schemas/schema";
+import { APMoveResult } from "../schemas/moveresults";
+import { RectGrid, reviver, UserFacingError } from "../common";
+import i18next from "i18next";
+import { UndirectedGraph } from "graphology";
+import { bidirectional } from "graphology-shortest-path";
+
+type playerid = 1 | 2;
+type PlayerLines = [string[], string[]];
+
+interface IMoveState extends IIndividualState {
+    currplayer: playerid;
+    board: Map<string, playerid>;
+    connPath: string[];
+    lastmove?: string;
+}
+
+export interface ITemplState extends IAPGameState {
+    winner: playerid[];
+    stack: Array<IMoveState>;
+};
+
+export class TemplGame extends GameBase {
+    public static readonly gameinfo: APGamesInformation = {
+        name: "Templ",
+        uid: "templ",
+        playercounts: [2],
+        version: "20240316",
+        dateAdded: "2024-02-13",
+        // i18next.t("apgames:descriptions.templ")
+        description: "apgames:descriptions.templ",
+        urls: [],
+        people: [],
+        variants: [
+            { uid: "size-9", group: "board" },
+        ],
+        categories: ["goal>connect", "mechanic>place", "board>shape>rect", "board>connect>rect", "components>simple"],
+        flags: ["experimental", "pie", "rotate90"],
+    };
+
+    public coords2algebraic(x: number, y: number): string {
+        return GameBase.coords2algebraic(x, y, this.boardSize);
+    }
+
+    public algebraic2coords(cell: string): [number, number] {
+        return GameBase.algebraic2coords(cell, this.boardSize);
+    }
+
+    public numplayers = 2;
+    public currplayer!: playerid;
+    public board!: Map<string, playerid>;
+    public connPath: string[] = [];
+    public gameover = false;
+    public winner: playerid[] = [];
+    public stack!: Array<IMoveState>;
+    public results: Array<APMoveResult> = [];
+    public variants: string[] = [];
+    private boardSize = 0;
+    private dots: string[] = [];
+    private grid: RectGrid;
+    private lines: [PlayerLines,PlayerLines];
+
+    constructor(state?: ITemplState | string, variants?: string[]) {
+        super();
+        if (state === undefined) {
+            if (variants !== undefined) {
+                this.variants = [...variants];
+            }
+            const fresh: IMoveState = {
+                _version: TemplGame.gameinfo.version,
+                _results: [],
+                _timestamp: new Date(),
+                currplayer: 1,
+                board: new Map(),
+                connPath: [],
+            };
+            this.stack = [fresh];
+        } else {
+            if (typeof state === "string") {
+                state = JSON.parse(state, reviver) as ITemplState;
+            }
+            if (state.game !== TemplGame.gameinfo.uid) {
+                throw new Error(`The Templ game code cannot process a game of '${state.game}'.`);
+            }
+            this.gameover = state.gameover;
+            this.winner = [...state.winner];
+            this.variants = state.variants;
+            this.stack = [...state.stack];
+        }
+        this.load();
+        this.grid = new RectGrid(this.boardSize, this.boardSize);
+        this.lines = this.getLines();
+    }
+
+    public load(idx = -1): TemplGame {
+        if (idx < 0) {
+            idx += this.stack.length;
+        }
+        if (idx < 0 || idx >= this.stack.length) {
+            throw new Error("Could not load the requested state from the stack.");
+        }
+
+        const state = this.stack[idx];
+        if (state === undefined) {
+            throw new Error(`Could not load state index ${idx}`);
+        }
+        this.results = [...state._results];
+        this.currplayer = state.currplayer;
+        this.board = new Map(state.board);
+        this.connPath = [...state.connPath];
+        this.lastmove = state.lastmove;
+        this.boardSize = this.getBoardSize();
+        return this;
+    }
+
+    private getLines(): [PlayerLines,PlayerLines] {
+        const lineN: string[] = [];
+        const lineS: string[] = [];
+        for (let x = 0; x < this.boardSize; x++) {
+            const N = this.coords2algebraic(x, 0);
+            const S = this.coords2algebraic(x, this.boardSize - 1);
+            lineN.push(N);
+            lineS.push(S);
+        }
+        const lineE: string[] = [];
+        const lineW: string[] = [];
+        for (let y = 0; y < this.boardSize; y++) {
+            const E = this.coords2algebraic(this.boardSize-1, y);
+            const W = this.coords2algebraic(0, y);
+            lineE.push(E);
+            lineW.push(W);
+        }
+        return [[lineN, lineS], [lineE, lineW]];
+    }
+
+    private getBoardSize(): number {
+        // Get board size from variants.
+        if (this.variants !== undefined && this.variants.length > 0 && this.variants[0] !== undefined && this.variants[0].length > 0) {
+            const sizeVariants = this.variants.filter(v => v.includes("size"))
+            if (sizeVariants.length > 0) {
+                const size = sizeVariants[0].match(/\d+/);
+                return parseInt(size![0], 10);
+            }
+            if (isNaN(this.boardSize)) {
+                throw new Error(`Could not determine the board size from variant "${this.variants[0]}"`);
+            }
+        }
+        return 13;
+    }
+
+    public moves(player?: playerid): string[] {
+        if (player === undefined) {
+            player = this.currplayer;
+        }
+        if (this.gameover) { return []; }
+        const moves: string[] = [];
+        return moves;
+    }
+
+    public randomMove(): string {
+        const moves = this.moves();
+        return moves[Math.floor(Math.random() * moves.length)];
+    }
+
+    public handleClick(move: string, row: number, col: number, piece?: string): IClickResult {
+        try {
+            const cell = this.coords2algebraic(col, row);
+            let newmove = "";
+            newmove = cell;
+            const result = this.validateMove(newmove) as IClickResult;
+            if (!result.valid) {
+                result.move = "";
+            } else {
+                result.move = newmove;
+            }
+            return result;
+        } catch (e) {
+            return {
+                move,
+                valid: false,
+                message: i18next.t("apgames:validation._general.GENERIC", { move, row, col, piece, emessage: (e as Error).message })
+            }
+        }
+    }
+
+    public validateMove(m: string): IValidationResult {
+        const result: IValidationResult = {valid: false, message: i18next.t("apgames:validation._general.DEFAULT_HANDLER")};
+        if (m.length === 0) {
+            result.valid = true;
+            result.complete = -1;
+            result.canrender = true;
+            result.message = i18next.t("apgames:validation.templ.INITIAL_INSTRUCTIONS");
+            return result;
+        }
+        result.valid = true;
+        result.complete = 1;
+        result.message = i18next.t("apgames:validation._general.VALID_MOVE");
+        return result;
+    }
+
+    public move(m: string, {partial = false, trusted = false} = {}): TemplGame {
+        if (this.gameover) {
+            throw new UserFacingError("MOVES_GAMEOVER", i18next.t("apgames:MOVES_GAMEOVER"));
+        }
+
+        let result;
+        m = m.toLowerCase();
+        m = m.replace(/\s+/g, "");
+        if (!trusted) {
+            result = this.validateMove(m);
+            if (!result.valid) {
+                throw new UserFacingError("VALIDATION_GENERAL", result.message);
+            }
+            if (!partial && !this.moves().includes(m)) {
+                throw new UserFacingError("VALIDATION_FAILSAFE", i18next.t("apgames:validation._general.FAILSAFE", { move: m }));
+            }
+        }
+        if (m.length === 0) { return this; }
+        this.dots = [];
+        this.results = [];
+        this.results.push({ type: "place", where: m });
+        this.board.set(m, this.currplayer);
+
+        this.lastmove = m;
+        this.currplayer = this.currplayer % 2 + 1 as playerid;
+
+        this.checkEOG();
+        this.saveState();
+        return this;
+    }
+
+    private buildGraph(player: playerid): UndirectedGraph {
+        const graph = new UndirectedGraph();
+        // seed nodes
+        [...this.board.entries()].filter(([,p]) => p === player).forEach(([cell,]) => {
+            graph.addNode(cell);
+        });
+        // for each node, check neighbours
+        // if any are in the graph, add an edge
+        for (const node of graph.nodes()) {
+            const [x,y] = this.algebraic2coords(node);
+            const neighbours = this.grid.adjacencies(x, y, false).map(n => this.coords2algebraic(...n));
+            for (const n of neighbours) {
+                if ( (graph.hasNode(n)) && (! graph.hasEdge(node, n)) ) {
+                    graph.addEdge(node, n);
+                }
+            }
+        }
+        return graph;
+    }
+
+    protected checkEOG(): TemplGame {
+        const otherPlayer = this.currplayer % 2 + 1 as playerid;
+        const graph = this.buildGraph(otherPlayer);
+        const [sources, targets] = this.lines[otherPlayer - 1];
+        for (const source of sources) {
+            for (const target of targets) {
+                if ( (graph.hasNode(source)) && (graph.hasNode(target)) ) {
+                    const path = bidirectional(graph, source, target);
+                    if (path !== null) {
+                        this.gameover = true;
+                        this.winner = [otherPlayer];
+                        this.connPath = [...path];
+                        break;
+                    }
+                }
+            }
+            if (this.gameover) {
+                break;
+            }
+        }
+        if (this.gameover) {
+            this.results.push({ type: "eog" });
+            this.results.push({ type: "winners", players: [...this.winner] });
+        }
+        return this;
+    }
+
+    public state(): ITemplState {
+        return {
+            game: TemplGame.gameinfo.uid,
+            numplayers: 2,
+            variants: this.variants,
+            gameover: this.gameover,
+            winner: [...this.winner],
+            stack: [...this.stack],
+        };
+    }
+
+    protected moveState(): IMoveState {
+        return {
+            _version: TemplGame.gameinfo.version,
+            _results: [...this.results],
+            _timestamp: new Date(),
+            currplayer: this.currplayer,
+            lastmove: this.lastmove,
+            board: new Map(this.board),
+            connPath: [...this.connPath],
+        };
+    }
+
+    public render(): APRenderRep {
+        // Build piece string
+        let pstr = "";
+        for (let row = 0; row < this.boardSize; row++) {
+            if (pstr.length > 0) {
+                pstr += "\n";
+            }
+            for (let col = 0; col < this.boardSize; col++) {
+                const cell = this.coords2algebraic(col, row);
+                if (this.board.has(cell)) {
+                    const contents = this.board.get(cell);
+                    if (contents === 1) {
+                        pstr += "A";
+                    } else if (contents === 2) {
+                        pstr += "B";
+                    }
+                } else {
+                    pstr += "-";
+                }
+            }
+        }
+        pstr = pstr.replace(new RegExp(`-{${this.boardSize}}`, "g"), "_");
+
+        // Build rep
+        const rep: APRenderRep =  {
+            board: {
+                style: "vertex",
+                width: this.boardSize,
+                height: this.boardSize,
+            },
+            legend: {
+                A: [{ name: "piece", player: 1 }],
+                B: [{ name: "piece", player: 2 }],
+            },
+            pieces: pstr,
+        };
+
+        // @ts-ignore
+        rep.annotations = [];
+        if (this.results.length > 0) {
+            for (const move of this.results) {
+                if (move.type === "place") {
+                    const [x, y] = this.algebraic2coords(move.where!);
+                    rep.annotations.push({ type: "enter", targets: [{ row: y, col: x }] });
+                } else if (move.type === "move") {
+                    const [fromX, fromY] = this.algebraic2coords(move.from);
+                    const [toX, toY] = this.algebraic2coords(move.to);
+                    rep.annotations.push({ type: "move", targets: [{ row: fromY, col: fromX }, { row: toY, col: toX }] });
+                }
+            }
+            if (this.connPath.length > 0) {
+                type RowCol = {row: number; col: number;};
+                const targets: RowCol[] = [];
+                for (const cell of this.connPath) {
+                    const [x,y] = this.algebraic2coords(cell);
+                    targets.push({row: y, col: x})
+                }
+                // @ts-ignore
+                rep.annotations.push({type: "move", targets, arrow: false});
+            }
+        }
+        if (this.dots.length > 0) {
+            const points = [];
+            for (const cell of this.dots) {
+                const [x, y] = this.algebraic2coords(cell);
+                points.push({ row: y, col: x });
+            }
+            // @ts-ignore
+            rep.annotations.push({ type: "dots", targets: points });
+        }
+        return rep;
+    }
+
+    public status(): string {
+        let status = super.status();
+
+        if (this.variants !== undefined) {
+            status += "**Variants**: " + this.variants.join(", ") + "\n\n";
+        }
+
+        return status;
+    }
+
+    public clone(): TemplGame {
+        return new TemplGame(this.serialize());
+    }
+}
